602aea67031fca0ba1765802af7f813d0dc5481c
Carlos Eduardo Witte <cewitte@outlook.com>
2021-06-16 16:10:56 -0300

    Using DTO design pattern to return users

--- src/main/java/com/github/cewitte/resources/UserResource.java
+++ src/main/java/com/github/cewitte/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.github.cewitte.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.cewitte.domain.User;
+import com.github.cewitte.dto.UserDTO;
 import com.github.cewitte.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
-		List<User> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<User> list = service.findAll();
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
  
