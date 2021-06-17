9b7b1a433de8883e82d24eba4178ab15c510ee62
Carlos Eduardo Witte <cewitte@outlook.com>
2021-06-17 10:33:39 -0300

    Getting Posts by id

---
+++ src/main/java/com/github/cewitte/services/PostService.java
@@ -0,0 +1,30 @@
+package com.github.cewitte.services;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.cewitte.domain.Post;
+import com.github.cewitte.repository.PostRepository;
+import com.github.cewitte.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Post post;
+
+		try {
+			post = repo.findById(id).get();
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("User object not found");
+		}
+
+		return post;
+	}
+
+}
